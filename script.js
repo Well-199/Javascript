23bd8ff7f220dc56288207a17ab3738236b7024c
Well-appedidos <wellington.lira@appedidos.com.br>
2022-12-06 11:46:16 -0300

    0.0.1

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 class Person {
+
+    name = ""
+    age = 0
+
     constructor(name, age){
         this.name = name
         this.age = age
     }
 }
+
+let pessoa1 = new Person("Well", 30)
+
+console.log(pessoa1.name+ " "+ pessoa1.age)
